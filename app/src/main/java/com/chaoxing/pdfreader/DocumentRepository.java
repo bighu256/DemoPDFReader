861593759e23ca42b1482d5578f05814b81bc8e2
bighu <bighu256@gmail.com>
2018-03-27 00:04:19 +0800

    night

--- app/src/main/java/com/chaoxing/pdfreader/DocumentRepository.java
+++ app/src/main/java/com/chaoxing/pdfreader/DocumentRepository.java
@@ -3,6 +3,14 @@ package com.chaoxing.pdfreader;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MediatorLiveData;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 /**
  * Created by HUWEI on 2018/3/26.
  */
@@ -19,8 +27,33 @@ public class DocumentRepository {
         return new DocumentRepository();
     }
 
-    public LiveData<DocumentBinding> loadDocument(String documentPath) {
-        
+    public LiveData<DocumentBinding> loadDocument(final String documentPath) {
+        Observable.create(new ObservableOnSubscribe<DocumentBinding>() {
+            @Override
+            public void subscribe(ObservableEmitter<DocumentBinding> emitter) throws Exception {
+
+            }
+        }).observeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<DocumentBinding>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(DocumentBinding documentBinding) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
         return result;
     }
 
